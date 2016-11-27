ce6fb0967ccc91410690787b51987512f60c32d3
Amir Ashtiani <pillowmermaid@gmail.com>
2016-11-27 15:00:41 -0500

    Functionality done
    Carousel is responsive for most common resolution targets
    Basic styling

--- app/static/main.js
+++ app/static/main.js
@@ -1,10 +1,32 @@
-var carouselContainer = $('.carousel__container');
-var carousel = $('.carousel');
-var carouselOrder = [];
-var currentSlide;
+var carouselContainer = $('.carousel__container'),
+		carousel 					= $('.carousel'),
+		desktopBreak 			= 1024,
+		tabletBreak 			= 768,
+		mobileBreak 			= 640,
+		slideWidth				= 0,
+		slideWidthMargin 	= 0,
+ 		carouselOrder 		= [],
+		currentSlide			= 0;
 
 function initCarousel(){
-	console.log('test');
+	window.addEventListener('resize', function(){
+		if (window.innerWidth >= 1300){
+			slideWidthMargin = 20;
+			setTimeout(resizeCarousel(4), 500);
+		}
+		else if (window.innerWidth < 1300 && window.innerWidth > 1024) {
+			slideWidthMargin = 20;
+			setTimeout(resizeCarousel(3), 500);
+		}
+		else if (window.innerWidth <= 1024 && window.innerWidth >= 768) {
+			slideWidthMargin = 10;
+			setTimeout(resizeCarousel(3), 500);
+		}
+		else {
+			slideWidthMargin = 10;
+			setTimeout(resizeCarousel(1), 500);
+		}
+	});
 	$.ajax({
 		dataType: 'jsonp',
     contentType: "application/jsonp; charset=utf-8",
@@ -17,40 +39,67 @@ function initCarousel(){
 				var runners = [];
 				sortedResults = data[i].results.sort(function(a,b){return parseFloat(a.votes)-(b.votes)}).reverse();
 				for(var j = 0; j < sortedResults.length; j++){
-					runners.push("<div class='runner "+sortedResults[j].partyCode.toLowerCase()+"'>"+sortedResults[j].name+"</div>");
+					runners.push(
+						"<div class='runner "+sortedResults[j].partyCode.toLowerCase()+"'><span class='name'>"+sortedResults[j].name+"</span><span class='votes'><span class='count'>"+sortedResults[j].votes+"</span> votes</span></div>"
+					);
 				}
 				var itemClass = 'item__'+id;
-				var carouselItem = "<li class='carousel__item "+itemClass+"'>"+name+"<div class='results'>"+runners.join('')+"</div></li>";
+				var carouselItem = "<li class='carousel__item "+itemClass+"'><h2>"+name+"</h2><div class='results'>"+runners.join('')+"</div></li>";
 
 				carouselOrder.push({"id": id, "target":itemClass});
 				carousel.append(carouselItem);
 			}
 			currentslide = 0;
-			var slideWidth = $('.carousel .carousel__item').outerWidth();
-			carouselContainer.css({'width': slideWidth*3+30+'px'});
-			carousel.css({'left': slideWidth*(-1)-10+'px'});
+			var cellCount;
+			if (window.innerWidth >= 1300){
+				slideWidthMargin = 20;
+				cellCount = 4;
+			}
+			else if (window.innerWidth < 1300 && window.innerWidth > 1024) {
+				slideWidthMargin = 20;
+				cellCount = 3;
+			}
+			else if (window.innerWidth <= 1024 && window.innerWidth >= 768) {
+				slideWidthMargin = 10;
+				cellCount = 3;
+			}
+			else {
+				slideWidthMargin = 10;
+				cellCount = 1;
+			}
+			resizeCarousel(cellCount);
 			$('.carousel .carousel__item:first').before($('.carousel .carousel__item:last'));
 
 		}
 	});
 }
 
+function resizeCarousel(numCells){
+		slideWidth = $('.carousel .carousel__item').outerWidth();
+		if (numCells > 1){
+			carouselContainer.css({'width': slideWidth*numCells+slideWidthMargin*numCells+'px'});
+		}
+		else {
+			carouselContainer.css({'width': '100%'});
+		}
+		carousel.css({'left': slideWidth*(-1)-slideWidthMargin+'px', 'width': slideWidth*carouselOrder.length});
+		console.log('resized to', window.innerWidth);
+}
+
 function nextClick(){
-	var slide = $('.carousel .carousel__item');
-	var spacing = slide.outerWidth() + 10;
+	var spacing = slideWidth + slideWidthMargin;
 	var leftIndent = parseInt(carousel.css('left')) - spacing;
 	carousel.animate({'left': leftIndent},200,'linear',function(){
-		carousel.css({'left': '-310px'});
+		carousel.css({'left': (-1)*spacing+'px'});
 			$('.carousel .carousel__item:last').after($('.carousel .carousel__item:first'));
 	});
 }
 
 function prevClick(){
-	var slide = $('.carousel .carousel__item');
-	var spacing = slide.outerWidth()  + 10;
+	var spacing = slideWidth  + slideWidthMargin;
 	var leftIndent = parseInt(carousel.css('left')) + spacing;
 	carousel.animate({'left': leftIndent},200, 'linear',function(){
-		carousel.css({'left': '-310px'});
+		carousel.css({'left': (-1)*spacing+'px'});
 			$('.carousel .carousel__item:first').before($('.carousel .carousel__item:last'));
 	});
 }
